709652925ee25815b7c604e67f05e9163c22e424
Oliver Mills <mills.oliver@ymail.com>
2020-11-14 11:54:38 -0500

    adding basic tower upgrades

--- Assets/Scripts/TurretBlueprint.cs
+++ Assets/Scripts/TurretBlueprint.cs
@@ -2,12 +2,24 @@
 // Copyright (c) GreedyGuppyGames. All rights reserved.
 // </copyright>
 
-using UnityEditor;
 using UnityEngine;
 
 [System.Serializable]
 public class TurretBlueprint
 {
     public GameObject prefab;
+    public GameObject prefabUpgrade01;
+    public GameObject prefabUpgrade02;
+    public GameObject prefabUpgrade03;
+    public GameObject prefabUpgrade10;
+    public GameObject prefabUpgrade20;
+    public GameObject prefabUpgrade30;
+
     public int cost;
+    public int upgradeCost01;
+    public int upgradeCost02;
+    public int upgradeCost03;
+    public int upgradeCost10;
+    public int upgradeCost20;
+    public int upgradeCost30;
 }
