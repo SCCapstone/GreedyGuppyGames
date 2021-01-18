bc34e13650b92cf447561a57a7726247b298c206
Jacob A. H <zark247@gmail.com>
2021-01-18 14:57:52 -0500

    Init for tests
    Init for Unit and Behavioral tests done, they just need to be written now.

---
+++ Assets/Tests/EditMode/WaveSpawnerTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests
+{
+    public class WaveSpawnerTest
+    {
+        [Test]
+        public void WaveSpawner()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
