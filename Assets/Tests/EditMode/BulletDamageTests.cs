d735ae91c180ee83159d1583aee2070113af8ea5
BennettSMarra <bmarra@email.sc.edu>
2021-01-22 12:57:48 -0500

    #116 Add framework for unit testing and initial bullet damage test
    Adds the needed assembly and script files for creating unit tests. Also includes a simple unit test that checks if how much damage the bullet does is correct.

---
+++ Assets/Tests/EditMode/BulletDamageTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BulletDamage
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BulletDamageValue()
+        {
+            // ACT
+            // GameObject testBulletGO = (GameObject)Instantiate(this.bulletPrefab, this.firePoint.position, this.firePoint.rotation);
+            // Bullet testBullet = testBulletGO.GetComponent<Bullet>();
+            Bullet testBullet = new Bullet(); // This is bad and technically shouldn't work, bullet is a monobehavior. Watch more Jason Weimann to fix
+            int standardBulletDamage = testBullet.damage;
+
+            // ASSERT
+            Assert.AreEqual(expected: 50, actual: standardBulletDamage);
+        }
+    }
+}
