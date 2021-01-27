bddda62dad082ec7aa39ae642096e22a7a1ceb41
BennettSMarra <bmarra@email.sc.edu>
2021-01-27 16:56:57 -0500

    Add interface and getters for the enemy class
    This is helpful for #116

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,9 @@
 
 using UnityEngine;
 using DG.Tweening;
-public class Enemy : MonoBehaviour
-{
+public class Enemy : MonoBehaviour, IEnemy {
     public Animator anim;
-    
+
     public float speed = 10f;
 
     public int health = 100;
@@ -19,49 +18,54 @@ public class Enemy : MonoBehaviour
     private Transform target;
     protected int wavepointIndex = 0;
 
-    public virtual void Start()
-    {
+    // Getters
+    public float GetSpeed() {
+        return this.speed;
+    }
+
+    public int GetHealth() {
+        return this.health;
+    }
+
+    public int GetValue() {
+        return this.value;
+    }
+
+    public virtual void Start() {
         this.target = Waypoints.points[wavepointIndex];
         transform.DOLookAt(new Vector3(target.position.x, transform.position.y, target.position.z), .25f);
         //anim.Play("Walk Forward Slow WO Root", -1, 0);
     }
 
-    public void TakeDamage(int amount)
-    {
+    public void TakeDamage(int amount) {
         this.health -= amount;
-        if (this.health <= 0)
-        {
+        if (this.health <= 0) {
             this.Die();
         }
     }
 
-    public virtual void Die()
-    {
+    public virtual void Die() {
         PlayerStats.Money += this.value;
         GameObject effect = (GameObject)Instantiate(this.deathEffect, this.transform.position, Quaternion.identity);
         Destroy(effect, 5f);
         Destroy(this.gameObject);
-        
+
     }
 
-    private void Update()
-    {
+    private void Update() {
         // Direction pointing to waypoint
         Vector3 dir = this.target.position - this.transform.position;
         this.transform.Translate(dir.normalized * this.speed * Time.deltaTime, Space.World);
-        
+
         // Checks if we are verrrrry close to a waypoint
-        if (Vector3.Distance(this.transform.position, this.target.position) <= 0.4f)
-        {
+        if (Vector3.Distance(this.transform.position, this.target.position) <= 0.4f) {
             this.GetNextWaypoint();
         }
     }
 
-    private void GetNextWaypoint()
-    {
+    private void GetNextWaypoint() {
         // Enemy has reached the end
-        if (this.wavepointIndex >= Waypoints.points.Length - 1)
-        {
+        if (this.wavepointIndex >= Waypoints.points.Length - 1) {
             this.EndPath();
             return; // makes sure the code doesn't skip into next node segment (yes this happens)
         }
@@ -70,14 +74,12 @@ public class Enemy : MonoBehaviour
         this.wavepointIndex++;
         this.target = Waypoints.points[this.wavepointIndex];
         // Look at waypoint, rotation stuff
-        transform.DOLookAt(new Vector3(target.position.x, transform.position.y, target.position.z ), .25f);
+        transform.DOLookAt(new Vector3(target.position.x, transform.position.y, target.position.z), .25f);
     }
 
-    public void SetWaypoint (int index)
-    {
+    public void SetWaypoint(int index) {
         // Enemy has reached the end
-        if (this.wavepointIndex >= Waypoints.points.Length - 1)
-        {
+        if (this.wavepointIndex >= Waypoints.points.Length - 1) {
             this.EndPath();
             return; // makes sure the code doesn't skip into next node segment (yes this happens)
         }
@@ -89,8 +91,7 @@ public class Enemy : MonoBehaviour
         transform.DOLookAt(new Vector3(target.position.x, transform.position.y, target.position.z), .25f);
     }
 
-    private void EndPath()
-    {
+    private void EndPath() {
         PlayerStats.Lives -= 25;
         Destroy(this.gameObject);
     }
---
+++ Assets/Scripts/IEnemy.cs
@@ -0,0 +1,9 @@
+﻿public interface IEnemy {
+    void Die();
+    void SetWaypoint(int index);
+    void Start();
+    void TakeDamage(int amount);
+    float GetSpeed();
+    int GetHealth();
+    int GetValue();
+}
