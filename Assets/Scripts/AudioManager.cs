2dc409ec668be1ebf7ad648dda28a09c9bffa893
Jacob A. H <zark247@gmail.com>
2021-02-05 15:34:20 -0500

    Revert to Beetle update

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,20 +5,10 @@ using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour
 {
     //An array for keep track of audio clips
-    public Sounds[] musicSounds;
-    public Sounds[] gameSFXSounds;
-    public Sounds[] menuSFXSounds;
-
+    public Sounds[] sounds;
 
     //Reference to the AudioManager itself
     public static AudioManager instance;
-    public AudioMixer audioMixer;
-    public AudioMixerGroup MusicMixerGroup;
-    public AudioMixerGroup SFXMixerGroup;
-    public string MusicMixerGroupName;
-    public string SFXMixerGroupName;
-    public string gameMusicName;
-
 
     void Awake()
     {
@@ -37,93 +27,37 @@ public class AudioManager : MonoBehaviour
         //Allows the AudioManager to transition between scenes
         DontDestroyOnLoad(gameObject);
 
-        /**
-         * Constructor for internal array for each sound in sounds
-         * adds the audio clip, volume, pitch, loop, and mixer group to each
-         * music group depends on if it is a sfx or music for the sliders
-         * */
-        foreach(Sounds s in musicSounds)
+        //Constructor for internal array for each sound in sounds
+        foreach(Sounds s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.audioClip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
-            s.source.outputAudioMixerGroup = MusicMixerGroup;
-        }
-
-        foreach (Sounds s in gameSFXSounds)
-        {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.audioClip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
-            s.source.outputAudioMixerGroup = SFXMixerGroup;
-        }
-
-        foreach (Sounds s in menuSFXSounds)
-        {
-            s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.audioClip;
-            s.source.volume = s.volume;
-            s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
-            s.source.outputAudioMixerGroup = SFXMixerGroup;
         }
     }
 
     void Start()
     {
         // Plays game music
-        PlayAudio(gameMusicName);
+        PlayAudio("GameMusic");
     }
 
     //Plays an audio clip
     public void PlayAudio(string audioName)
     {
         //Searches the array of sounds for the name of the passed in audio clip
-        Sounds s = Array.Find(musicSounds, sound => sound.name == audioName);
+        Sounds s = Array.Find(sounds, sound => sound.name == audioName);
         
         //if not found displays a warning
-        if(s != null)
+        if(s == null)
         {
-            //if found plays the audio
-            s.source.Play();
+            Debug.LogWarning("Sound: " + audioName + " not found");
             return;
         }
 
-        s = Array.Find(gameSFXSounds, sound => sound.name == audioName);
-
-        //if not found displays a warning
-        if (s != null)
-        {
-            //if found plays the audio
-            s.source.Play();
-            return;
-        }
-
-        s = Array.Find(menuSFXSounds, sound => sound.name == audioName);
-
-        //if not found displays a warning
-        if (s != null)
-        {
-            //if found plays the audio
-            s.source.Play();
-            return;
-        }
-        Debug.Log("Sound not found");
-    }
-
-    // sets music volume for slider using logarithmic scale
-    public void SetMusicVolume(float volume)
-    {
-        audioMixer.SetFloat(MusicMixerGroupName, Mathf.Log10(volume) * 20);
-    }
-
-    // sets sfx volume for slider using logarithmic scale
-    public void SetSFXVolume(float volume)
-    {
-        audioMixer.SetFloat(SFXMixerGroupName, Mathf.Log10(volume) * 20);
+        //if found plays the audio
+        s.source.Play();
     }
 }
