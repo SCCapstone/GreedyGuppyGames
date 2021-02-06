a674030fcd4f578b9f1c86b65d0f3d40572fe0d0
Jacob A. H <zark247@gmail.com>
2021-02-06 14:46:45 -0500

    Added in a scene for the main menu
    Basics for the main menu are implemented. Need to add functionality to the main scene.
    
    Removed a lot of unused resources: example scenes

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -15,6 +15,13 @@ public class AudioManager : MonoBehaviour
 
     //Reference to the AudioManager itself
     public static AudioManager instance;
+    public AudioMixer audioMixer;
+    public AudioMixerGroup MusicMixerGroup;
+    public AudioMixerGroup SFXMixerGroup;
+    public string MusicMixerGroupName;
+    public string SFXMixerGroupName;
+    public string gameMusicName;
+
 
     void Awake()
     {
@@ -33,14 +40,39 @@ public class AudioManager : MonoBehaviour
         //Allows the AudioManager to transition between scenes
         DontDestroyOnLoad(gameObject);
 
-        //Constructor for internal array for each sound in sounds
-        foreach(Sounds s in sounds)
+        /**
+         * Constructor for internal array for each sound in sounds
+         * adds the audio clip, volume, pitch, loop, and mixer group to each
+         * music group depends on if it is a sfx or music for the sliders
+         * */
+        foreach(Sounds s in musicSounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.audioClip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = MusicMixerGroup;
+        }
+
+        foreach (Sounds s in gameSFXSounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.audioClip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = SFXMixerGroup;
+        }
+
+        foreach (Sounds s in menuSFXSounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.audioClip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = SFXMixerGroup;
         }
     }
 
@@ -56,16 +88,47 @@ public class AudioManager : MonoBehaviour
     public void PlayAudio(string audioName)
     {
         //Searches the array of sounds for the name of the passed in audio clip
-        Sounds s = Array.Find(sounds, sound => sound.name == audioName);
-
+        Sounds s = Array.Find(musicSounds, sound => sound.name == audioName);
+        
         //if not found displays a warning
-        if(s == null)
+        if(s != null)
         {
-            Debug.LogWarning("Sound: " + audioName + " not found");
+            //if found plays the audio
+            s.source.Play();
             return;
         }
 
-        //if found plays the audio
-        s.source.Play();
+        s = Array.Find(gameSFXSounds, sound => sound.name == audioName);
+
+        //if not found displays a warning
+        if (s != null)
+        {
+            //if found plays the audio
+            s.source.Play();
+            return;
+        }
+
+        s = Array.Find(menuSFXSounds, sound => sound.name == audioName);
+
+        //if not found displays a warning
+        if (s != null)
+        {
+            //if found plays the audio
+            s.source.Play();
+            return;
+        }
+        Debug.Log("Sound not found");
+    }
+
+    // sets music volume for slider using logarithmic scale
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat(MusicMixerGroupName, Mathf.Log10(volume) * 20);
+    }
+
+    // sets sfx volume for slider using logarithmic scale
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat(SFXMixerGroupName, Mathf.Log10(volume) * 20);
     }
 }
