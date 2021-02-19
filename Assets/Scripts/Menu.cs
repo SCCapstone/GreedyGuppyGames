c2c97b817aadb8a2dec01ca4e836147b43b45db9
Jacob A. H <zark247@gmail.com>
2021-02-19 15:19:55 -0500

    Changed Main Menu to panels
    Added Panels for the settings, achievements, level select and main menu.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,6 +7,50 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MenuFunctions
 {
+
+    public GameObject MainPanel;
+    public GameObject SettingsPanel;
+    public GameObject AchievementsPanel;
+    public GameObject LevelSelectPanel;
+
+    void Start()
+    {
+        MainPanel.SetActive(true);
+        SettingsPanel.SetActive(false);
+        AchievementsPanel.SetActive(false);
+        LevelSelectPanel.SetActive(false);
+    }
+    public void ShowSettingsPanel()
+    {
+        MainPanel.SetActive(false);
+        SettingsPanel.SetActive(true);
+        AchievementsPanel.SetActive(false);
+        LevelSelectPanel.SetActive(false);
+    } 
+    public void ShowAchievementsPanel()
+    {
+        MainPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        AchievementsPanel.SetActive(true);
+        LevelSelectPanel.SetActive(false);
+    }
+    public void ShowLevelSelectPanel()
+    {
+        MainPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        AchievementsPanel.SetActive(false);
+        LevelSelectPanel.SetActive(true);
+    }
+
+
+
+
+
+
+
+
+
+
     //Clicking Play will advance the player to the Level select scene(when it is finished, currently advances to the play scene)
     public void StartGame()
     {
