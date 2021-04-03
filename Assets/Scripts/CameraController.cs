b90ffc19f1f4e09bf1b4aae2807985dc47b11bfb
Oliver Mills <mills.oliver@ymail.com>
2021-04-03 11:01:48 -0400

    camera controller added
    camera doesnt clamp vertically correctly yet. clamping needs work overall

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 30f;
+    public float panBorderThickness= 10f;
+
+    public float scrollSpeed = 5000f;
+    public float minY = 20f;
+    public float maxY = 80f;
+    public float minX = 0f;
+    public float maxX = 75f;
+    public float minZ = 0f;
+    public float maxZ = 75f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+        if(Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if(Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if(Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+
+        transform.position = pos;
+
+    }
+}
