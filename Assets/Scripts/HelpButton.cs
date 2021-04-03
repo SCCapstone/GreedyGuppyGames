5a6ae1af1cfa1a994be59a7acab49bf47c24c61b
Bennett Marra <bmarra@email.sc.edu>
2021-04-03 18:00:04 -0400

    Add help menu to all levels
    Prefab overlay canvas is not actually used but I used it to roughly figure out how to edit the ui correctly.

---
+++ Assets/Scripts/HelpButton.cs
@@ -0,0 +1,57 @@
+﻿// <copyright file="HelpButton.cs" company="GreedyGuppyGames">
+// Copyright (c) GreedyGuppyGames. All rights reserved.
+// </copyright>
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Pauses time when the player clicks help and brings up the help menu
+public class HelpButton : MenuFunctions
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject HelpMenuUI;
+    public GamerManager gamerManager;
+
+    private void Start()
+    {
+        HelpMenuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !gamerManager.gameEnded)
+        {
+            if(GameIsPaused)
+            {
+                Unpause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // Stops time
+    public void Pause()
+    {
+        Debug.Log("Game Paused");
+        HelpMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    // Resumes time
+    public void Unpause()
+    {
+        Debug.Log("Game unpaused");
+        HelpMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+}
+
